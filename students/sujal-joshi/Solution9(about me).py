75f69f4daa5584997ae2eea5ee7ff530f3bf21ba
Sujal joshi <1sujal7j2006@gmail.com>
2026-06-12 12:06:59 +0530

    Update Solution9(about me).py

--- students/sujal-joshi/Solution9(about me).py
+++ students/sujal-joshi/Solution9(about me).py
@@ -1,8 +1,46 @@
+#problem1
 def about_me():
     name = input("Enter your name: ")
     age = int(input("Enter your age: "))
     city = input("Enter your city: ")
     language = input("Enter your favourite programming language: ")
+    years = int(input("How many years have you been coding? "))
+
+    print("\n--- About Me ---")
+    print(f"Name: {name}")
+    print(f"Age: {age}")
+    print(f"City: {city}")
+    print(f"Favourite Programming Language: {language}")
+    print(f"Years of Coding Experience: {years}")
+    print("----------------")
+
+about_me()
+
+
+#problem2
+def about_me():
+
+    # Name validation
+    while True:
+        name = input("Enter your name: ")
+        if name.strip() == "":
+            print("Name can't be empty — try again.")
+        else:
+            break
+
+    # Age validation
+    while True:
+        age_input = input("Enter your age: ")
+
+        if age_input.isdigit():
+            age = int(age_input)
+            break
+        else:
+            print("Please type a whole number.")
+
+    city = input("Enter your city: ")
+    language = input("Enter your favourite programming language: ")
+
     years = int(input("How many years have you been coding? "))
 
     print("\n--- About Me ---")
